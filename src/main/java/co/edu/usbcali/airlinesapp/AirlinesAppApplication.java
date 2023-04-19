b2902176a051749014eef483a608fb2a5288c7a0
Jurgen Sanclemente <jurgensanclemente@gmail.com>
2023-04-18 22:56:05 -0500

    Add .jar file

--- src/main/java/co/edu/usbcali/airlinesapp/AirlinesAppApplication.java
+++ src/main/java/co/edu/usbcali/airlinesapp/AirlinesAppApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AirlinesAppApplication {
+
+
+	public String PORT = System.getenv("PORT");
+
 	public static void main(String[] args) {
 		SpringApplication.run(AirlinesAppApplication.class, args);
 	}
